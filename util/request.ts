c4ec966c0873a88893f42d8cc2922c58baf3433b
hanlianhao <hanlianhao@meituan.com>
2019-10-14 15:20:05 +0800

    change hostname with node_env

--- util/request.ts
+++ util/request.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import qs from 'query-string';
-
-export const host = 'http://localhost:4000';
+export const host = `http://${process.env.NODE_ENV === 'development' ? 'localhost' : 'blog.godhaoye.com'}:4000`;
 
 export function fetchData(url: string, query: any = {}) {
   return axios(`${host}/api${url}${qs.stringify(query)}`);
