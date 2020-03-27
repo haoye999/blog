a6ae800dd15e2e022005f6b22f402fb8f8d8772c
hanlianhao <haoye999han@gmail.com>
2020-03-27 17:24:22 +0800

    feat: 时间修改

--- pages/post/[name].tsx
+++ pages/post/[name].tsx
@@ -1,15 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
-import ReactMarkdown from 'react-markdown';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import CodeBlock from '../../components/CodeBlock';
-import Layout from '../../components/MyLayout';
-import { Post } from '../../interfaces';
-import { parseDate, host } from '../../util';
-import './index.less';
+import axios, { AxiosResponse } from "axios";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { NextPage } from "next";
+import Head from "next/head";
+import CodeBlock from "../../components/CodeBlock";
+import Layout from "../../components/MyLayout";
+import { Post } from "../../interfaces";
+import { parseDate, host } from "../../util";
+import "./index.less";
 
 const PostComponent: NextPage<Post> = props => {
   const { title, birthtime, mtime, _content, description } = props;
+
+  const [showUpdate, setShowUpdate] = useState(true);
 
   return (
     <>
@@ -18,21 +21,32 @@ const PostComponent: NextPage<Post> = props => {
       </Head>
       <Layout>
         <article>
-          <h1 className='title'>{title}</h1>
-          <h2 className='description'>
-            <ReactMarkdown
-              className='markdown'
-              source={description}
-            />
+          <h1 className="title">{title}</h1>
+          <h2 className="description">
+            <ReactMarkdown className="markdown" source={description} />
           </h2>
-          <span className='refresh-time'>更新于：{parseDate(mtime)}</span>
+          {showUpdate ? (
+            <span
+              className="refresh-time"
+              onClick={() => setShowUpdate(!showUpdate)}
+            >
+              更新于：{parseDate(mtime)}
+            </span>
+          ) : (
+            <span
+              className="publish-time"
+              onClick={() => setShowUpdate(!showUpdate)}
+            >
+              发布于：{parseDate(birthtime)}
+            </span>
+          )}
           <hr />
           <section>
             <ReactMarkdown
-              className='markdown'
+              className="markdown"
               source={_content}
               renderers={{
-                code: CodeBlock,
+                code: CodeBlock
               }}
             />
           </section>
@@ -40,12 +54,12 @@ const PostComponent: NextPage<Post> = props => {
       </Layout>
     </>
   );
-}
+};
 
 PostComponent.getInitialProps = async ctx => {
   const { name } = ctx.query;
   const res: AxiosResponse<Post> = await axios(`${host}/api/post/${name}`);
   return res.data;
-}
+};
 
 export default PostComponent;
--- util/request.ts
+++ util/request.ts
@@ -1,6 +1,9 @@
-import axios from 'axios';
-import qs from 'query-string';
-export const host = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://blog.godhaoye.com';
+import axios from "axios";
+import qs from "query-string";
+export const host =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://blog.godhaoye.com";
 
 export function fetchData(url: string, query: any = {}) {
   return axios(`${host}/api${url}${qs.stringify(query)}`);
