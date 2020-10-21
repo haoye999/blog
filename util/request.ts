985662d4ef8103b453847cb945d5f42b256530fd
hanlianhao <hanlianhao.coder@bytedance.com>
2020-10-21 11:09:00 +0800

    fix: 域名

--- util/request.ts
+++ util/request.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 import qs from "query-string";
-export const host =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000"
-    : "https://blog.godhaoye.com";
 
 export function fetchData(url: string, query: any = {}) {
-  return axios(`${host}/api${url}${qs.stringify(query)}`);
+  return axios(`/api${url}${qs.stringify(query)}`);
 }
